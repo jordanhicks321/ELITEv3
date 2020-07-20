243d275b09ef46131a6b467a711858b97aef4b06
Jordan Hicks <jordan.adam.hicks@gmail.com>
2020-07-20 09:54:57 -0400

    Initial Commit

---
+++ ELITEv3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ELITEv3
+//
+//  Created by Dj Haven on 7/20/20.
+//  Copyright © 2020 Dj Haven. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
